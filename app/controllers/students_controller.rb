18149d31d9f689c23a97891b333832b9b10b08a8
te68 <te68@cornell.edu>
2019-06-28 13:40:09 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,21 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    first_name = params[:student][:first_name]
+    last_name = params[:student][:last_name]
+    student = Student.create(first_name: first_name, last_name: last_name)
+    redirect_to student_path(student)
   end
 
 end
